0e62500b1d32cd437c128eb8bbd1068d3b55730a
La3eeb <54325963+Omar-abubakr@users.noreply.github.com>
2022-03-18 21:26:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const { Client, Intents, Collection} = require('discord.js');
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const prefix = '!';
+const fs = require('fs');
+ 
+bot.commands = new Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    bot.commands.set(command.name, command);
+}
+
+bot.once('ready' , () => {
+    console.log('TutBot is Ready!')
+})
+
+bot.on("message", msg => {
+    if (msg.author.bot) return
+
+    else if (msg.content === "hi") {
+        msg.reply("Hello 👋")
+    }
+    else if (msg.content === "ping")
+         msg.channel.send("Pong !")
+})
+
+bot.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        bot.commands.get('ping').execute(message, args);
+    } 
+    else if(command === 'youtube'){
+        bot.commands.get('youtube').execute(message, args);
+    }
+}),
+bot.login("OTE1MTkwMTc3MDc1NDM3NTY4.YaX_RQ.ido58WrNTJm4KGQBWf-NbBJZAjY")
