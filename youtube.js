c3ba6f749934b68e5d67512faadc229f89c7eec0
La3eeb <54325963+Omar-abubakr@users.noreply.github.com>
2022-03-18 21:28:58 +0300

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "youtube",
+    description: "Send My YouTube Chanel Link",
+    execute(message, args){
+        message.channel.send("https://www.youtube.com/channel/UCHOPO3hUzd0QEuhljIYLh0Q");
+    }
+}
